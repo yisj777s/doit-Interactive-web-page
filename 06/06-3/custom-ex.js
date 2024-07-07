4904d607cdfe6ef48ee877787d2d084022b7974b
yisj777s <yisj777s@naver.com>
2024-07-07 09:59:16 +0900

    add 06-3.자바스크립트로 스타일 제어하기

---
+++ 06/06-3/custom-ex.js
@@ -0,0 +1,9 @@
+// 06.자바스크립트로 웹 페이지 제어하기
+// 06-3.자바스크립트로 스타일 제어하기
+const box = document.querySelector("#box");
+
+box.style.width = "10%";
+box.style.height = "300px";
+box.style.backgroundColor = "hotpink";
+box.style.border = "none";
+box.style.transform = "rotate(10deg)";
