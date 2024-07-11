d105a496fad1ecfbc8c53b381814fa86d3804830
yisj777s <yisj777s@naver.com>
2024-07-11 11:21:39 +0900

    add 06-6.함수를 활용하여 코드 패키징하기

---
+++ 06/06-6/codepackaging-ex.js
@@ -0,0 +1,14 @@
+// 06.자바스크립트로 웹 페이지 제어하기
+// 06-6.함수를 활용하여 코드 패키징하기
+// function plus(){
+//   console.log(2+3);
+// }
+
+// plus();
+
+
+function plus(num1, num2){
+  console.log(num1+num2);
+}
+
+plus(2,3);
